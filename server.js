0ce9ac85ba1583f4a4552d18b1a5e7e3046d609a
Diogo Souza <diogosouzac@gmail.com>
2020-08-14 09:20:02 -0300

    removing cors

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-const http = require("http");
-const express = require("express");
-var cors = require("cors");
+var express = require("express"),
+  bodyParser = require("body-parser");
 
 const app = express();
-app.use(express.json());
-
-// enabling cors
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 app.use("/books", require("./routes/books"));
 
-app.use("/", function (req, res) {
-	res.send("Hey, I'm here! :)");
-});
-
-const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
-server.listen(PORT);
+app.listen(PORT);
+
 console.debug("Server listening on port: " + PORT);
